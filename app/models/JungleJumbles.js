d70390bca21c6ac82c201b94515e69158e8009dd
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-29 13:54:16 -0600

    Added JunbleJumble array data into the AppState

--- app/models/JungleJumbles.js
+++ app/models/JungleJumbles.js
@@ -5,6 +5,7 @@ export class JungleJumble {
     this.id = generateId()
     this.name = data.name
     this.body = data.body
+
     // to best keep track of the fastest times you might want these properties too! They would start null cause no one has completed these yet.
     this.fastestTime = null
     this.startTime = null
