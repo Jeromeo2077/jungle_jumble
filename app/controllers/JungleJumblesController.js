f3438e1f60391b5fc68a43c97d2f6cc86b073a2f
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-29 14:19:51 -0600

    Created the drawJungleJumbles function on the JungleJumblesController

--- app/controllers/JungleJumblesController.js
+++ app/controllers/JungleJumblesController.js
@@ -1,6 +1,19 @@
+import { AppState } from "../AppState.js";
+import { setHTML } from "../utils/Writer.js";
+
 export class JungleJumblesController {
   constructor() {
     console.log('JungleJumblesController has loaded');
 
+  }
+
+  drawJungleJumbles() {
+    const jungleJumbles = AppState.jumbles
+
+    let jungleJumblesHTML = ''
+
+    jungleJumbles.forEach(jungleJumble => jungleJumblesHTML += jungleJumble.ListTemplate)
+
+    setHTML('junglejumbleslist', jungleJumblesHTML)
   }
 }
